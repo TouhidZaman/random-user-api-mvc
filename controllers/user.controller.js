d40f9549a9d5d9c294d490c061b7d0d7fa1e4d9c
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2023-05-02 22:49:20 +0600

    random user api added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,3 +13,17 @@ module.exports.getAllUser = (req, res) => {
     res.status(200).send(users);
   });
 };
+
+module.exports.getRandomUser = (req, res) => {
+  fs.readFile("./users.json", "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+      res.status(501).send({
+        message: "Internal Server Error",
+      });
+    }
+    const users = JSON.parse(data);
+    const randomUserIndex = Math.floor(Math.random() * users.length);
+    res.status(200).send(users[randomUserIndex]);
+  });
+};
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userController = require("../../controllers/user.controller");
 const router = express.Router();
 
-router.route("/all").get(userController.getAllUser);
+router.get("/all", userController.getAllUser);
+router.get("/random", userController.getRandomUser);
 
 module.exports = router;

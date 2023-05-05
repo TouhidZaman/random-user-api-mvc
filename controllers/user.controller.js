a36b577d77d40bf6785b11e2b7e0d3ba086b2c47
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2023-05-05 23:56:50 +0600

    file path adjusted

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
+const path = require("path");
 
 module.exports.getAllUser = (req, res) => {
   const limit = req.query.limit;
-  fs.readFile("users.json", "utf8", (error, data) => {
+  fs.readFile(path.join(process.cwd(), "users.json"), "utf8", (error, data) => {
     if (error) {
       console.log(error);
       res.status(501).send({
@@ -15,7 +16,7 @@ module.exports.getAllUser = (req, res) => {
 };
 
 module.exports.getRandomUser = (req, res) => {
-  fs.readFile("users.json", "utf8", (error, data) => {
+  fs.readFile(path.join(process.cwd(), "users.json"), "utf8", (error, data) => {
     if (error) {
       console.log(error);
       res.status(501).send({
